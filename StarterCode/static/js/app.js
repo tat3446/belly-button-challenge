f55b0a1dec1a15bc9ca168b4626937572f6c6746
tat3446 <127369366+tat3446@users.noreply.github.com>
2023-08-16 21:38:23 -0500

    updated code with charts and metadata

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,79 +1,108 @@
-//initialize
-//function init() 
-
 // Use D3 library to read in JSON
 const bellyb = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-  // Fetch the JSON data and console log it
-  d3.json(bellyb).then(function(data) {
-    getData(data.names);
-    console.log(data.names);
-  });
+// Fetch the JSON data and console log it
+d3.json(bellyb).then(function (data) {
+  console.log(data)
+  getData(data.names);
+  buildCharts(data.names[0]);
+  buildMetaData(data.names[0]);
+});
+
+function buildMetaData(value) {
+  d3.json(bellyb).then(function (data) {
+
+    // Set variables
+    let metadata = data.metadata;
+    let resultArray = metadata.filter(sampleObj => sampleObj.id == value);
+    let result = resultArray[0];
+    console.log(result)
+    let box = d3.select("#sample-metadata");
+    box.html("");
+
+    Object.entries(result).forEach(([key, value]) => {
+      box.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+  })
+}
+
+
 
 
 // Function called by DOM changes to select person
 function getData(names) {
 
   // Select dropdown
-  console.log(names[0]);
-
-  let dropdownMenu = d3.select("#selDataset"); 
+  let dropdownMenu = d3.select("#selDataset");
   // For loop to go through all names
-  for (let i = 0; i < names.length;i++) {
+  for (let i = 0; i < names.length; i++) {
     dropdownMenu.append("option")
-    .text(names[i])
-    .property("value", names[i]);
-    // console.log(dropdownMenu);
+      .text(names[i])
+      .property("value", names[i]);
+
   };
-
-
 };
 
-// // On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
+// Function called when dropdown number is changed to switch data
+function optionChanged(id) {
+  buildCharts(id)
+  buildMetaData(id)
+}
+
+function buildCharts(value) {
+  d3.json(bellyb).then(function (data) {
 
 
-// console.log(test);
+    // Set variables
+    let samples = data.samples;
+    let resultArray = samples.filter(sampleObj => sampleObj.id == value);
+    let result = resultArray[0];
+    console.log(result)
 
-// // Sort the OTU data
-// let sortOTUdata = samples.sort((a,b) => b.sample_values - a.sample_values),
-// //Slice the top 10 OTUs
-// slicedData = sortOTUdata.slice(0,10),
-// // Reverse the array to accommodate Plotly's defaults
-// reversedData = slicedData.reverse(),
-// // Trace1 for one plot
-// let trace1 = {
-//   x: reversedData.map(object => object.sample_values),
-//   y: reversedData.map(object => object.otu_ids),
-//   text: reversedData.map(object => object.otu_labels),
-//   name: "OTUs",
-//   type: "bar",
-//   orientation: "h"
-// };
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
 
-// // Data array
-// let traceData = [trace1];
 
-// // Apply a title to the layout
-// let layout = {
-//   title: "Top 10 OTUs found for individual selected",
-//   margin: {
-//     l: 100,
-//     r: 100,
-//     t: 100,
-//     b: 100
-//   }
-// };
+    // Trace1 for one plot
+    let trace1 = [{
+      y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h",
+    }];
 
-// // Render the plot to the div tag with id "plot"
-// Plotly.newPlot("plot", traceData, layout);
+    let layout1 = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
 
-// Call function to update the chart
-//updatePlotly(data);
+    Plotly.newPlot("bar", trace1, layout1);
 
-// Update the restyled plot's values
-// function updatePlotly(newdata) {
-//   Plotly.restyle("plot", "values", [newdata]);
-// }
+    let bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
 
-//init();
+    let bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
+
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+  })
+};
